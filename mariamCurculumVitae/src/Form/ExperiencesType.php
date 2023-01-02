031722b5875754a0c5cd0257be541ac98d67b372
Mariam <82433410+Mariam-26@users.noreply.github.com>
2023-01-02 16:43:31 +0100

    Create ExperiencesType.php

---
+++ mariamCurculumVitae/src/Form/ExperiencesType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Experiences;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ExperiencesType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('titre')
+            ->add('sous_titre')
+            ->add('description')
+            ->add('date_debut')
+            ->add('date_fin')
+            ->add('utilisateur')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Experiences::class,
+        ]);
+    }
+}
